122aba72d54fabcf9ce35b3ebb025f5eaa6dfb07
Mariana González <gonzalez.marianav19@gmail.com>
2024-04-19 22:40:47 -0600

    Linkedlist y logica timer

---
+++ src/LinkedList.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+
+class Node {
+    int value;
+    Node next;
+
+    public Node(int value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+public class LinkedList {
+    private Node head;
+    private int size;
+
+    public void SinglyLinkedList() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    public void clear() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public boolean contains(int element) {
+        Node current = this.head;
+        while (current != null) {
+            if (current.value == element) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    public boolean add(int element) {
+        Node newNode = new Node(element);
+        if (this.head == null) {
+            this.head = newNode;
+        } else {
+            Node current = this.head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        this.size++;
+        return true;
+    }
+
+    public int remove(int element) {
+        if (this.head == null) {
+            throw new NoSuchElementException();
+        }
+        if (this.head.value == element) {
+            this.head = this.head.next;
+            this.size--;
+            return element;
+        }
+        Node current = this.head;
+        while (current.next != null) {
+            if (current.next.value == element) {
+                current.next = current.next.next;
+                this.size--;
+                return element;
+            }
+            current = current.next;
+        }
+        throw new NoSuchElementException();
+    }
+}
